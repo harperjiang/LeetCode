cafc9b812001f566846ddf1d04cbaccb93a3ef11
Hao Jiang <harperjiang@msn.com>
2022-12-03 00:38:26 -0500

    Daily Leet

--- src/find_the_duplicate_number/Solution.java
+++ src/find_the_duplicate_number/Solution.java
@@ -2,7 +2,29 @@ package find_the_duplicate_number;
 
 public class Solution {
     public int findDuplicate(int[] nums) {
-        int mask = 0;
-        for()
+        int[] bits = new int[32];
+        int[] should = new int[32];
+
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = 0; j < 32; j++) {
+                should[j] += (i & (1 << j)) != 0 ? 1 : 0;
+            }
+        }
+
+        for (int num : nums) {
+            for (int i = 0; i < 32; i++) {
+                bits[i] += (num & (1 << i)) != 0 ? 1 : 0;
+            }
+        }
+        int sum = 0;
+        for (int i = 0; i < 32; i++) {
+            if (bits[i] > should[i])
+                sum += (1 << i);
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        new Solution().findDuplicate(new int[]{2, 2, 2, 2, 2});
     }
 }
