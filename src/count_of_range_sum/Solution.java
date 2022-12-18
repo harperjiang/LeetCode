4258fa9328102ed710258a57499983fe243f81aa
harper <harperjiang@msn.com>
2022-12-18 11:21:07 -0500

    Daily Leet

--- src/count_of_range_sum/Solution.java
+++ src/count_of_range_sum/Solution.java
@@ -2,6 +2,12 @@ package count_of_range_sum;
 
 public class Solution {
     public int countRangeSum(int[] nums, int lower, int upper) {
-        return 0;
+        int counter = 0;
+        long sum = 0;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().countRangeSum(new int[]{-2147483647, 0, -2147483647, 2147483647}, -564, 3864));
     }
 }
